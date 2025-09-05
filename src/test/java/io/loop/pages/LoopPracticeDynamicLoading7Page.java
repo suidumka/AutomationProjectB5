9fa1e9a9f51bbc22e0c186bf5b4cdd364c8e8355
suidumka <jalilova.s.m@gmail.com>
2025-09-04 21:07:53 -0400

    wait + action

---
+++ src/test/java/io/loop/pages/LoopPracticeDynamicLoading7Page.java
@@ -0,0 +1,23 @@
+package io.loop.pages;
+
+import io.loop.test.Utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoopPracticeDynamicLoading7Page {
+    public LoopPracticeDynamicLoading7Page() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy (id = "alert")
+    public WebElement doneMsg;
+
+    @FindBy (xpath= "//img[@src='/img/a-few-moments.jpg']")
+    public WebElement image;
+
+    @FindBy (xpath ="//a[@href='dynamic_loading/7.html']")
+    public WebElement getDynamicLoading7;
+
+
+}
